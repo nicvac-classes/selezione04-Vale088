81fea10c308eb70acc64a4cf3c3ec64489dc4421
Vale088 <valerio.sinni@marconibari.edu.it>
2024-09-30 14:58:17 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,36 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int num1,num2,num3,max;
+    cout<<"inserisci il primo valore"<<endl; 
+    cin>>num1;
+    cout<<"inserisci il secondo valore"<<endl;
+    cin>>num2;
+    cout<<"inserisci il terzo valore"<<endl;
+    cin>>num3;
+    if(num1>num2) 
+    { 
+    if(num1>num3) 
+     
+    
+    {     
+    max=num1; 
+    }
+    else 
+    { 
+    max=num3;
+     }  
+     if(num2>num3)
+     {
+    max=num2
+    }     
+     else 
+    {    
+     max=num3;
+    }
+      } 
+      cout<<"il numero massimo e " << & max<<endl; 
 }
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
